d58f3cba33c1b6068ee01c45a4844262fd2f4750
Reiq.Laptop <reiqvideos@gmail.com>
2021-07-21 05:08:39 -0400

    Los productos se volvieron a unificar para mayor facilidad al publicarlos, carousel de todos los productos en funcionamiento

--- js/script.js
+++ js/script.js
@@ -1,36 +1,37 @@
 /* VARIABLES DEL DOM */
 var contenedor_productos = document.getElementById('contenedor-productos');
-var contenedor_complementos = document.getElementById('contenedor-complementos');
+var productos;
 
 /* OBTENIENDO TODOS LOS PRODUCTOS GUARDADOS EN EL JSON */
-function ObtenerProductosJSON(url_json, contenedor){
+function ObtenerProductosJSON(contenedor){
     axios({
         method: 'GET',
-        url: url_json
+        url: 'json/productos.json'
     }).then(res => {
-        imprimirTarjetas(res.data, contenedor, res.data.length, res.data[0].tipo)
+        productos = res.data;
+        imprimirTarjetas(res.data, contenedor, res.data.length);
     }).catch(err => {
         console.log('Hay un error con los productos!', err);
     });
 }
 
 // CREANDO LAS FILAS PARA AGREGAR LOS PRODUCTOS Y COMPLEMENTOS EN VENTA
-function creandoFilas(contenedor, n_articulos, tipo_articulo){
+function creandoFilas(contenedor, n_articulos){
     var cantidad_filas = Math.ceil(n_articulos / 3);
     for(var i = 1; i <= cantidad_filas; i++){
-        contenedor.innerHTML += `<div class="row" id="${tipo_articulo}-fila-${i}"></div>`;
+        contenedor.innerHTML += `<div class="row" id="productos-fila-${i}"></div>`;
     }
 }
 
 //IMPRESION DE LAS TARJETAS
-function imprimirTarjetas(articulo, contenedor, n_articulos, tipo_articulo){
-    creandoFilas(contenedor, n_articulos, tipo_articulo);
+function imprimirTarjetas(articulo, contenedor, n_articulos){
+    creandoFilas(contenedor, n_articulos);
     var cantidad_filas = Math.ceil(n_articulos / 3);
     var id = 0;
     for(var i = 1; i <= cantidad_filas; i++){
         for(var j = 1; j <= 3; j++){
             if(articulo[id] != undefined){
-                document.getElementById(tipo_articulo + '-fila-' + i).innerHTML += `
+                document.getElementById('productos-fila-' + i).innerHTML += `
                 <div class="col-md-4 my-3">
                     <div class="card">
                     <img src="${articulo[id].carpeta}1.jpg" alt="${articulo[id].nombre}" class="card-img-top">
@@ -38,7 +39,7 @@ function imprimirTarjetas(articulo, contenedor, n_articulos, tipo_articulo){
                         <h5 class="card-title">${articulo[id].nombre}</h5>
                         <span class="badge badge-success w-100 mb-2"><h5 class="m-0">RD$ ${articulo[id].precio.toLocaleString('en-US')}</h5></span>
                         <p class="card-text">${articulo[id].descripcion}</p>
-                        <button class="btn btn-info w-100 my-2">Ver fotos</button>
+                        <button onclick="verFotos(${id})" class="btn btn-info w-100 my-2" data-toggle="modal" data-target="#Modal">Ver fotos</button>
                         <button class="btn btn-success w-100 my-2">Comprar <i class="fab fa-whatsapp"></i></button>
                     </div>
                     </div>
@@ -51,5 +52,31 @@ function imprimirTarjetas(articulo, contenedor, n_articulos, tipo_articulo){
 }
 
 //SE DEBE SEÑALAR LA RUTA DEL ARCHIVO JSON Y EL CONTENEDOR DONDE SE VAN A INTEGRAR LOS ARTICULOS
-ObtenerProductosJSON('json/productos.json', contenedor_productos);
-ObtenerProductosJSON('json/complementos.json', contenedor_complementos);
+ObtenerProductosJSON(contenedor_productos);
+
+//VARIABLES DEL DOM
+var titulo_modal = document.getElementById('ModalLabel');
+var contenedor_imagenes_carousel = document.getElementById('carousel-inner');
+
+function verFotos(id){
+    titulo_modal.innerText = productos[id].nombre;
+    contenedor_imagenes_carousel.innerHTML = '';
+    //DESACTIVANDO LOS CONTROLES DE CAROUSEL CUANDO SOLO HAY UNA IMAGEN
+    if(productos[id].cantidad_fotos == 1){
+        document.getElementById('contenedor-controles-carousel').style.display = "none";
+    } else {
+        document.getElementById('contenedor-controles-carousel').style.display = "block";
+    }
+    for(var i = 1; i <= productos[id].cantidad_fotos; i++){
+        if(i == 1){
+            var active = 'active';
+        } else {
+            var active = '';
+        }
+        contenedor_imagenes_carousel.innerHTML += `
+            <div class="carousel-item ${active}">
+                <img src="${productos[id].carpeta + i}.jpg" class="d-block w-100" alt="${productos[id].nombre}">
+            </div>
+        `;
+    }
+}
